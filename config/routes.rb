a6cddb5f0b872c5fcf66c7c24d9b6386d3243120
slackcode at school <slackcode@gmail.com>
2011-09-20 09:38:44 +0800

    finish sorcery example

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Researcher::Application.routes.draw do
-  get "sessions/new"
+  resources :posts
+  resources :users
+  resources :sessions
 
+  get "sessions/new"
   get "users/new"
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +58,7 @@ Researcher::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110919160613) do
+ActiveRecord::Schema.define(:version => 20110920002829) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username",                     :null => false
