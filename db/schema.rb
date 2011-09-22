ffd31a358a2c94abdeb4cfec7452a284a4520e24
slackcode home <slackcode@gmail.com>
2011-09-23 00:48:34 +0800

    add rails_kindeditor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110922022612) do
+ActiveRecord::Schema.define(:version => 20110922163617) do
+
+  create_table "kindeditor_assets", :force => true do |t|
+    t.string   "asset"
+    t.integer  "file_size"
+    t.string   "file_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
