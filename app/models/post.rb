25e8b840b150044c75ca64dcbb3e9b25ebd8e23f
slackcode at school <slackcode@gmail.com>
2011-09-23 15:45:53 +0800

    Revert "set default value"
    This reverts commit a5be25f3569eb2988d0fa707f28766128db7e3ec.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,3 @@
 class Post < ActiveRecord::Base
-  after_initialize :default_value
-
-  def default_value
-    if self.title.nil?
-      self.bk_purpose_importance = I18n.t('bk_purpose_importance_html')
-    end
-  end
+  #:title => "asdf"
 end
