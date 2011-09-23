1290c3040ea17e9557bcef1c1aac80adae33a59c
slackcode at school <slackcode@gmail.com>
2011-09-23 15:46:08 +0800

    Revert "Revert "set default value""
    This reverts commit 25e8b840b150044c75ca64dcbb3e9b25ebd8e23f.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
-  #:title => "asdf"
+  after_initialize :default_value
+
+  def default_value
+    if self.title.nil?
+      self.bk_purpose_importance = I18n.t('bk_purpose_importance_html')
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110922163617) do
+ActiveRecord::Schema.define(:version => 20110923040759) do
 
   create_table "kindeditor_assets", :force => true do |t|
     t.string   "asset"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(:version => 20110922163617) do
     t.string   "subject",               :default => ""
     t.string   "school",                :default => ""
     t.string   "research_time",         :default => ""
-    t.text     "bk_purpose_importance", :default => ""
+    t.string   "bk_purpose_importance", :default => "a"
     t.text     "content_question",      :default => ""
     t.text     "cond_outcome",          :default => ""
     t.text     "feature_creation",      :default => ""
